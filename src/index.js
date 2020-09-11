decdf3fddf19ddb290377ffc1f77a5f6f8583c69
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-11 14:06:53 +0300

    do exercises

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 /*
 What makes this function a *component* is the fact that
 it returns something that React can render.
 That something is JSX.
 */
-function Hi() {
-  return <div>Hello world!</div>;
+function App() {
+  return (
+    <div className="container">
+      Hello, <strong>John Doe</strong>!
+      <hr />
+      <h4>What would you like to do today?</h4>
+      <ul>
+        <li>work</li>
+        <li>relax</li>
+        <li>do sports</li>
+      </ul>
+      <hr />
+      <table>
+        <thead>
+          <tr>
+            <th>Task ID</th>
+            <th>Task Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>17</td>
+            <td>Meet friend for lunch</td>
+          </tr>
+          <tr>
+            <td>30</td>
+            <td>Cook dinner</td>
+          </tr>
+          <tr>
+            <td>33</td>
+            <td>Do laundry</td>
+          </tr>
+        </tbody>
+      </table>
+      <hr />
+      <h4>Is 4 + 10 equal to (computed_in_curly_braces=){5 + 10}?</h4>
+    </div>
+  );
 }
 
 /*
@@ -19,4 +56,4 @@ gets the returned JSX,
 and inserts the corresponding HTML elements into the document
 under the element with id “root”. 
 */
-ReactDOM.render(<Hi />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
