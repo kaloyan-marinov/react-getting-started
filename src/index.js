e35384be5e0d2569b4421e3cf2a417051b1dec0b
Kaloyan Marinov <kallo.marinov@gmail.com>
2020-09-11 15:41:52 +0300

    undo all exercises

--- src/index.js
+++ src/index.js
@@ -2,49 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-/*
-When React renders a component, it passes the component’s props (short for “properties”)
-as the first argument, as an object. The props object is just a plain JS object...
-
-Inside a component that receives multiple props,
-each one will be a separate property on the “props” object that’s passed in.
-*/
-const App = ({ firstName, lastName }) => {
-  /*
-  This is JavaScript’s destructuring syntax (added in ES6), not a React thing.
-
-  What that says is: “I expect the first argument will be an object.
-  Please extract the ‘firstName’ and 'lastName' properties out of it,
-  and give them to me as variable called ‘firstName’ and 'lastName', respectively.”
-  */
-  return (
-    <div className="container">
-      Hello, {firstName} {lastName}!
-    </div>
-  );
+const Room = () => {
+  return <div className="room">The lights in this room are ON.</div>;
 };
 
-const MediaCard = ({ title, body, imageUrl }) => {
-  // Can't return all 3 elements at once - need to wrap them in a single parent element!
-  return (
-    <div>
-      <h2>{title}</h2>
-      <p>{body}</p>
-      <img src={imageUrl} />
-    </div>
-  );
-};
-
-const dogImage =
-  "./static/depositphotos_44339023-stock-photo-american-staffordshire-terrier-sitting-isolated.jpg";
-
-const Gate = ({ isOpen }) => {
-  return <div>This gate is {isOpen ? "OPEN" : "CLOSED"}.</div>;
-};
-
-/*
-You might then wonder, where do props come from? How do we pass them in?
-
-You, the developer, get to pass props to a component when it is rendered.
-*/
-ReactDOM.render(<Gate isOpen={false} />, document.querySelector("#root"));
+ReactDOM.render(<Room />, document.getElementById("root"));
